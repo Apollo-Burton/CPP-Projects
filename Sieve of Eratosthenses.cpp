7d1d5a109d5b7d3a3b539b9d5f20de1732d760cf
Apollo Burton <124854003+Apollo-Burton@users.noreply.github.com>
2023-02-08 22:28:41 -0800

    Create Sieve of Eratosthenses.cpp

---
+++ Sieve of Eratosthenses.cpp
@@ -0,0 +1,71 @@
+#include <iostream>  
+#include <vector>
+
+std::vector<int> sieve_of_eratosthenes(int max) {
+        
+    std::vector<int> primes;
+    std::vector<bool> nums(max, true);
+
+    for (int i = 2; i*i <= max; i++) { 
+        
+        if (nums[i]) {
+
+            for (int p = i*i; p <= max; p += i) {
+
+                nums[p] = false;
+
+            } 
+        
+        }
+
+    }
+
+    for (int i = 2; i <= max; i++) {
+       
+        if (nums[i]) {
+            
+            primes.push_back(i); 
+
+        }
+
+    }
+    return primes;
+
+}
+
+int main() { 
+    
+    int input;
+    std::string pref;
+    std::cout << "Range: 1, ";
+    std::cin >> input;
+
+    if (input <= 2000000000) {
+        
+        std::vector<int> primes = sieve_of_eratosthenes(input); 
+
+        if (input <= 1000000) {
+
+            std::cout << "Would you like to see the list of primes? (Y/n) ";
+            std::cin >> pref;
+
+            if (pref == "Y") {
+
+                for (int i = 0; i < primes.size(); i++) {
+                
+                    std::cout << primes[i] << "\n"; 
+
+                }
+            
+            }
+        
+        }
+        std::cout << "There are " << primes.size() << " prime numbers between 1 and " << input;
+    
+    } else {
+
+        std::cout << "Input must be <= 2,000,000,000";
+
+    }
+
+}
