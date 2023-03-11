69ce86d8d3eac938fd1bd963004d1624123266cf
Apollo Burton <124854003+Apollo-Burton@users.noreply.github.com>
2023-03-11 01:13:05 -0800

    Update quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,3 +1,5 @@
+// Broken as fuck
+
 #include <iostream>
 #include <vector>
 
@@ -9,17 +11,27 @@ void swap(int &a, int &b)
 	b = temp;
 }
 
-std::vector<int> quick_sort(std::vector<int> &vec)
+std::vector<std::vector<int>> partition(std::vector<int> vec)
 {
+	if (vec.size() == 1) {
+	    return {{vec}, {vec}, {0, vec[0]}};
+	}
+	if (vec.size() == 0) {
+	    return {{0}, {0}, {0, 0}};
+	}
 	// Initializing variables
+	std::vector<int> vec1;
+	std::vector<int> vec2;
 	int pivot = vec.size() / 2;
+	int	back = vec.size() - 1;
 	int pointerl = 0;
-	int pointerh = vec.size() - 1;
+	int pointerh = vec.size() - 2;
 	bool pointingl = false;
 	bool pointingh = false;
-	
-	// Running quick sort
-	while(pointerl < pointerh || pointerh > pointerl) {
+	swap(vec[back], vec[pivot]);
+	swap(back, pivot);
+
+	while (pointerl <= pointerh) {
 		if (vec[pointerl] > vec[pivot]) {
 			pointingl = true;
 		}
@@ -31,13 +43,16 @@ std::vector<int> quick_sort(std::vector<int> &vec)
 			pointingl = false;
 			pointingh = false;
 		}
-		if (pointingl == false && (pointerl) - 1 < pivot) {
+		if (pointingl == false) {
 			pointerl++;
 		}
-		if (pointingh == false && (pointerh) + 1 > pivot) {
+		if (pointingh == false) {
 			pointerh--;
 		}
 	}
+	// swap(back, pivot);
+	// swap(vec[back], vec[pivot]);
+	
 	if (pointingl) {
 		swap(vec[pointerl], vec[pivot]);
 		pivot = pointerl;
@@ -46,59 +61,56 @@ std::vector<int> quick_sort(std::vector<int> &vec)
 		swap(vec[pointerh], vec[pivot]);
 		pivot = pointerh;
 	}
-	vec.push_back(pivot);
-	return vec;
+	for (int i = 0; i < pivot; i++) {
+		vec1.push_back(vec[i]);
+	}
+	for (int i = pivot + 1; i < vec.size(); i++) {
+		vec2.push_back(vec[i]);
+	}
+	return {{vec1}, {vec2}, {pivot, vec[pivot]}};
 }
 
-std::vector<int> recursion(std::vector<int> &vec)
+std::vector<int> recursion(std::vector<int> input)
 {
-	quick_sort(vec);
-	int pivot = vec[vec.size() - 1];
-	int sections = 1;
-	std::vector<int> section;
-	std::vector<int> sorted_vec;
-	bool sorted = false;
-
-	while (sorted == false) {
-		sorted = true;
-		for (int i = 0; i < vec.size(); i += 2) {
-			if (vec[i] > vec[i + 1]) {
-				// Change to "sorted = false" once recursion is figured out
-				sorted = true;
-			}
-		}
-		for (int i = 0; i < pivot; i++) {
-		    section.push_back(vec[i]);
-		}
-		quick_sort(section);
-		
-		for (int i = 0; i < section.size() - 1; i++) {
-		    sorted_vec.push_back(section[i]);
-		}
-		section.clear();
-	}
+	std::vector<std::vector<int>> vec = partition(input);
+	std::vector<int> vec1 = vec[0];
+	std::vector<int> vec2 = vec[1];
+	std::vector<int> sorted(input.size());
+	int sorted_size = 1;
+	sorted[vec[2][0]] = vec[2][1];
 	
-	return sorted_vec;
+	while (sorted_size <= input.size() && vec[0].size() > 1) {
+		if (vec[0].size() > 1) {
+		    vec = partition(vec1);
+		    vec1 = vec[0];
+		}
+		sorted[vec[2][0]] = vec[2][1];
+		sorted_size++;
+		
+		if (vec[1].size() > 1) {
+            vec = partition(vec2);
+            vec2 = vec[1];
+		}
+		sorted[vec[2][0]] = vec[2][1];
+        sorted_size++;
+	}
+	return sorted;
 }
 
 int main()
 {
-	std::vector<int> vec = {3, 2, 5, 0, 1, 8, 7, 6, 9, 4};
-
+	std::vector<int> vec = {4, 8, 6, 2, 5, 8, 45, 3, 7, 3, 7, 9, 1, 34, 2, 15, 7, 5};
+	
 	std::cout << "Unsorted: ";
 	for (int i : vec) {
 		std::cout << i << " ";
 	}
 	std::cout << "\n";
 
-    std::vector<int> test = quick_sort(vec);
+	std::vector<int> sorted = recursion(vec);
 
 	std::cout << "  Sorted: ";
-	for (int i : test) {
+	for (int i : sorted) {
 		std::cout << i << " ";
 	}
 }
-// int sections *= 2 everytime quick_sort() is called
-// for (section : sections) {
-//     quick_sort(section);
-// }
